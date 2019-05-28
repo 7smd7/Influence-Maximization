e9ac21f91ce6825b50be79cd49ae2e69317b7e38
7smd7 <32780546+7smd7@users.noreply.github.com>
2019-05-29 03:30:51 +0430

    deep influence

--- main.py
+++ main.py
@@ -4,22 +4,40 @@ import matplotlib.pyplot as plt
 g = nx.DiGraph()
 
 n = 7  # 10 nodes
-m = 20  # 20 edges
+m = 10  # 20 graph
 
 g = nx.gnm_random_graph(n, m,directed=True)
 
-edges={n: adj for n, adj in g.adjacency()}
-nodes=list(g.nodes())
+graph={n: adj for n, adj in g.adjacency()}
+nodes=list(graph.keys())
+lenNodes=len(graph.keys())
 
-max=0
-a=0
-b=0
-for i in range(0,len(nodes)):
-    for j in range(i+1,len(g.nodes)):
-        intersection=list(set(edges[i]) & set(edges[j]))
-        n=len(intersection)
-        if n>max:
-            a,b,max=i,j,n
-print(a,b,max)
+intersections={}
+for i in range(0,lenNodes):
+    intersections[i]={}
+    for j in range(0,lenNodes):
+        intersection=list(set(graph[i]) & set(graph[j]))
+        intersections[i][j]=intersection
+
+activeNode={}
+# deactiveNode= set(nodes)
+
+def checkActivity( activeNode, m, n ):
+    activeNode={m,n}.union(activeNode)
+    toActive=set(intersections[m][n]) - activeNode
+    merge=activeNode.union(toActive)
+    ans=merge
+    if toActive==[]:
+        return []
+    for i in range (0,len(toActive)):
+        for j in range (0,len(activeNode)):
+            ans=ans.union(checkActivity(merge,list(toActive)[i],list(activeNode)[j]))
+        for j in range (i+1, len(toActive)):
+            ans=ans.union(checkActivity(merge,list(toActive)[i],list(toActive)[j]))
+    return ans
+
 nx.draw(g , with_labels=True)
 plt.show()
+a=checkActivity(activeNode,nodes[0],nodes[1])
+print(a)
+print(intersections[nodes[0]][nodes[1]])
